548e1e03b47315b20fefe450d695d2c03e96812b
mihai <cos_mihai23@yahoo.com>
2018-07-05 14:56:57 +0200

    Modify package.json again

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict'
+const gulp = require('gulp');
+
+gulp.task('default', function() {
+  console.log('this is a gulp task');
+});
+
+gulp.task('html', function() {
+  console.log('this is the html task')
+});
