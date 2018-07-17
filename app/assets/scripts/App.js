6dceb371816004dd80f2e9bf6670bb67663379b8
mihai <cos_mihai23@yahoo.com>
2018-07-17 14:25:19 +0200

    light the links when the specific section is scrolled

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/Reveal-on-scroll';
+import StikyHeader from './modules/StikyHeader';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "70%");
+var stikyHeader = new StikyHeader();
 
