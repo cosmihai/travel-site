59171a8d70f66fa1da60d233cb5484b1e6a8d219
mihai <cos_mihai23@yahoo.com>
2018-07-17 16:58:18 +0200

    completed the moda overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,11 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/Reveal-on-scroll';
 import StikyHeader from './modules/StikyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "70%");
 var stikyHeader = new StikyHeader();
+var modal = new Modal();
 
